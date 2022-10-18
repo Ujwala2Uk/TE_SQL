8d2e8e8e780ba3da4e5c1d10642e9b427615840c
Ujwala2Uk <roopaumesha2@gmail.com>
2022-10-18 17:23:43 +0530

    commit

---
+++ Kanopi_PersonalInfo.sql
@@ -0,0 +1,15 @@
+create database Kanopi_app
+use Kanopi_app
+create table kanopi_page
+(
+Id int primary key Identity(1,1),
+FirstName varchar(25),
+LastName varchar(25),
+Email varchar(25)
+)
+drop table kanopi_page
+select * from kanopi_page
+set identity_insert kanopi_page on
+
+
+
